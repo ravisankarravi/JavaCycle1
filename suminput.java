984040eeac2569292ffd9dc199e7dfbb7ce99017
ravisankarravi <77284840+ravisankarravi@users.noreply.github.com>
2022-01-06 23:21:47 +0530

    Add files via upload

---
+++ suminput.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class suminput
+{
+	public static void main(String[] args)
+{
+	Scanner object = new Scanner(System.in);
+	int a,b,sum;
+	System.out.println("Enter the numbers");
+	a=object.nextInt();
+	b=object.nextInt();
+	sum=a+b;
+	System.out.println("sum="+sum);
+	}
+}
