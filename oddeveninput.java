0cd737b93dde9974c6c4729fffffde43dafbe339
ravisankarravi <77284840+ravisankarravi@users.noreply.github.com>
2022-01-08 21:11:14 +0530

    Add files via upload

---
+++ oddeveninput.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class oddeveninput
+{
+	public static void main(String args[])
+	{
+	Scanner sc = new Scanner (System.in);
+	int a;
+	System.out.println("Enter the number");
+	a=sc.nextInt();
+	if(a%2==0)
+	{
+		System.out.println("Number is even");
+	}
+	else
+	{
+		System.out.println("Number is odd");
+	}
+	}
+}
+
+
